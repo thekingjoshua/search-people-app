2f11f356bfdafa906dd7b28ecd9065dde1a9d242
KingJoshua <kingjayeriughe@gmail.com>
2024-03-14 13:49:28 +0100

    Update useFilterUsers.js
    Memoized the filteredUsers to avoid performance issues when filtering the names of a lot of people, the filter logic will only run if the users and the searchValue changes.

--- src/hooks/useFilterUsers.js
+++ src/hooks/useFilterUsers.js
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 function useFilterUsers (users){
     const [searchValue, setSearchValue] = useState('');
 
-    const filteredUsers = users.filter(
-        user =>
+    const filteredUsers = useMemo(() => {
+      return users.filter( user =>
           user.firstName.toLowerCase().includes(searchValue.toLocaleLowerCase()) ||
           user.lastName.toLowerCase().includes(searchValue.toLocaleLowerCase()) ||
           user.username.toLowerCase().includes(searchValue.toLocaleLowerCase())
-      );
+      ); 
+    }, [users, searchValue]) 
 
       return {filteredUsers, searchValue, setSearchValue}
 } 
