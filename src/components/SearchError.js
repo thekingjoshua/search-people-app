630bd7b91d6e8e4b625e3addfdd63d875e7778ed
KingJoshua <kingjayeriughe@gmail.com>
2024-03-14 11:20:32 +0100

    Update SearchError.js
    Used the Error Theme

--- src/components/SearchError.js
+++ src/components/SearchError.js
@@ -1,5 +1,7 @@
+import { ColorTheme } from "./ui/theme"
+
 function SearchError({errorText}){
-    return <h1 style={{textAlign: 'center'}} >{errorText}</h1>
+    return <h1 style={{textAlign: 'center', color: ColorTheme.error['base']}} >{errorText}</h1>
 }
 
 export default SearchError
