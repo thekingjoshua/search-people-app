4e7d3823bdfd4ca53e7a25c2765313d79f0621b0
KingJoshua <kingjayeriughe@gmail.com>
2024-03-14 13:56:33 +0100

    Update useFilterUsers.js
    Defined the lowercased string of the search value in one variable

--- src/hooks/useFilterUsers.js
+++ src/hooks/useFilterUsers.js
@@ -2,14 +2,16 @@ import { useState, useMemo } from "react";
 
 function useFilterUsers (users){
     const [searchValue, setSearchValue] = useState('');
+    const searchValueLowerCase = searchValue.toLowerCase()
 
     const filteredUsers = useMemo(() => {
       return users.filter( user =>
-          user.firstName.toLowerCase().includes(searchValue.toLocaleLowerCase()) ||
-          user.lastName.toLowerCase().includes(searchValue.toLocaleLowerCase()) ||
-          user.username.toLowerCase().includes(searchValue.toLocaleLowerCase())
+          user.firstName.toLowerCase().includes(searchValueLowerCase) ||
+          user.lastName.toLowerCase().includes(searchValueLowerCase) ||
+          user.username.toLowerCase().includes(searchValueLowerCase)
       ); 
     }, [users, searchValue]) 
+
 
       return {filteredUsers, searchValue, setSearchValue}
 } 
